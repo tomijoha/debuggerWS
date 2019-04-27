dd5ce526d7d82ffeced389c37392ead5312a41fd
Tomi Johansson <tomppa152466@hotmail.com>
2019-04-27 13:35:49 +0300

    initial

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+});
+
+app.get('/debug', (req, res) => {
+    res.send('TODO on later')
+  });
+
+app.listen(8000, () => {
+  console.log('Example app listening on port 8000!')
+});
