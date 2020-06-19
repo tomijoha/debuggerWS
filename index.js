a8629dae4d97257be6648d2e13224bb6df963512
tomijoha <tomppa152466@hotmail.com>
2020-06-20 00:08:38 +0300

    Test

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello World again!')
+  res.send('Hello World!')
 });
 
 app.get('/debug', (req, res) => {
